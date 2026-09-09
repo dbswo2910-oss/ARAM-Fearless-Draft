cae8d6237d283c02964be48f8cd94e7b20de660d
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-09 13:29:10 +0900

    Expose Riot Grade collector IPC v0.15.28

---
+++ update/v0.15.28/preload.js
@@ -0,0 +1,15 @@
+'use strict';
+const { contextBridge, ipcRenderer } = require('electron');
+contextBridge.exposeInMainWorld('aramDesktop', {
+  isElectron: true,
+  getAutoSyncState: () => ipcRenderer.invoke('autosync:get-state'),
+  getAramMatchHistory: options => ipcRenderer.invoke('match-history:load', options || {}),
+  getRiotGradeState: () => ipcRenderer.invoke('riot-grade:get-state'),
+  pollRiotGrade: () => ipcRenderer.invoke('riot-grade:poll'),
+  getDesktopInfo: () => ipcRenderer.invoke('desktop:get-info'),
+  checkAndApplyUpdate: () => ipcRenderer.invoke('desktop:update-now'),
+  getItemCatalog: () => ipcRenderer.invoke('desktop:get-item-catalog'),
+  setAlwaysOnTop: value => ipcRenderer.invoke('desktop:set-always-on-top', !!value),
+  setLaunchAtStartup: value => ipcRenderer.invoke('desktop:set-launch-at-startup', !!value),
+  showWindow: () => ipcRenderer.invoke('desktop:show-window')
+});
