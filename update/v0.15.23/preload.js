71674b0490b5097241c92ac44d8cf8a279c722c4
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-09 11:45:10 +0900

    Add v0.15.23 preload updater bridge

---
+++ update/v0.15.23/preload.js
@@ -0,0 +1,12 @@
+'use strict';
+const { contextBridge, ipcRenderer } = require('electron');
+contextBridge.exposeInMainWorld('aramDesktop', {
+  isElectron: true,
+  getAutoSyncState: () => ipcRenderer.invoke('autosync:get-state'),
+  getAramMatchHistory: options => ipcRenderer.invoke('match-history:load', options || {}),
+  getDesktopInfo: () => ipcRenderer.invoke('desktop:get-info'),
+  checkAndApplyUpdate: () => ipcRenderer.invoke('desktop:update-now'),
+  setAlwaysOnTop: value => ipcRenderer.invoke('desktop:set-always-on-top', !!value),
+  setLaunchAtStartup: value => ipcRenderer.invoke('desktop:set-launch-at-startup', !!value),
+  showWindow: () => ipcRenderer.invoke('desktop:show-window')
+});
