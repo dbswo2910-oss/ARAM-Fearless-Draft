5aba73f5d104ca913630787d0df6dd056b7cd902
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-09 14:43:49 +0900

    Add v0.15.32 preload snapshot IPC

---
+++ update/v0.15.32/preload.js
@@ -0,0 +1,16 @@
+'use strict';
+const { contextBridge, ipcRenderer } = require('electron');
+contextBridge.exposeInMainWorld('aramDesktop', {
+  isElectron: true,
+  getAutoSyncState: () => ipcRenderer.invoke('autosync:get-state'),
+  getAramMatchHistory: options => ipcRenderer.invoke('match-history:load', options || {}),
+  getRiotGradeState: () => ipcRenderer.invoke('riot-grade:get-state'),
+  pollRiotGrade: () => ipcRenderer.invoke('riot-grade:poll'),
+  annotateRiotGradeSnapshots: rows => ipcRenderer.invoke('riot-grade:annotate-snapshots', Array.isArray(rows)?rows:[]),
+  getDesktopInfo: () => ipcRenderer.invoke('desktop:get-info'),
+  checkAndApplyUpdate: () => ipcRenderer.invoke('desktop:update-now'),
+  getItemCatalog: () => ipcRenderer.invoke('desktop:get-item-catalog'),
+  setAlwaysOnTop: value => ipcRenderer.invoke('desktop:set-always-on-top', !!value),
+  setLaunchAtStartup: value => ipcRenderer.invoke('desktop:set-launch-at-startup', !!value),
+  showWindow: () => ipcRenderer.invoke('desktop:show-window')
+});
