172b4a88e57cc6638b70e71a93c11986ad29b3e8
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 16:42:29 +0900

    refactor: expose RANDOM IN GAME volatile HUD shadow

--- src/random/ingame/index.js
+++ src/random/ingame/index.js
@@ -1,4 +1,5 @@
 'use strict';
 const contract=require('./contract');
 const semantic=require('./semantic-signature');
-module.exports={contract,semantic,production_active:false,score_logic_changed:false,random_scoring_changed:false};
+const volatileHud=require('./volatile-hud');
+module.exports={contract,semantic,volatileHud,production_active:false,score_logic_changed:false,random_scoring_changed:false};
