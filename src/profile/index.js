16241e69648d98a1dd49027972fc81a9b8bdedf6
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 19:14:57 +0900

    stability: expose canonical Profile Riot Grade link shadow

--- src/profile/index.js
+++ src/profile/index.js
@@ -3,4 +3,5 @@ const contract=require('./contract');
 const results=require('./results-normalizer');
 const metrics=require('./metrics');
 const history=require('./history-service');
-module.exports={contract,results,metrics,history,production_active:false,score_logic_changed:false,random_scoring_changed:false};
+const riotGradeLink=require('./riot-grade-link');
+module.exports={contract,results,metrics,history,riotGradeLink,production_active:false,score_logic_changed:false,random_scoring_changed:false};
