c54368c5733e58bf8318626ee363261ebf47dd6e
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 18:11:45 +0900

    stability: expose canonical item catalog service shadow

--- src/items/index.js
+++ src/items/index.js
@@ -5,4 +5,5 @@ const recommendation=require('./recommendation');
 const artResolver=require('./art-resolver');
 const artRuntime=require('./art-runtime');
 const catalogContract=require('./catalog-contract');
-module.exports={contract,identity,recommendation,artResolver,artRuntime,catalogContract,production_active:false,score_logic_changed:false,random_scoring_changed:false};
+const catalogService=require('./catalog-service');
+module.exports={contract,identity,recommendation,artResolver,artRuntime,catalogContract,catalogService,production_active:false,score_logic_changed:false,random_scoring_changed:false};
