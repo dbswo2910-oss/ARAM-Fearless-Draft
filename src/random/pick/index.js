2b58881cd92ec7e7fe9b6c2c5c98f2b397dacc65
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 16:33:41 +0900

    refactor: expose RANDOM PICK partial shadow modules

---
+++ src/random/pick/index.js
@@ -0,0 +1,5 @@
+'use strict';
+const contract=require('./contract');
+const dna=require('./dna');
+const selection=require('./selection-state');
+module.exports={contract,dna,selection,production_active:false,score_logic_changed:false,random_scoring_changed:false};
