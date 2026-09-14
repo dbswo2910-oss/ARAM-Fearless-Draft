ead8c065f871d6ed9778b0fe90c070e8c8f18282
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 16:44:40 +0900

    refactor: expose RANDOM IN GAME coordinator shadow

--- src/random/ingame/index.js
+++ src/random/ingame/index.js
@@ -2,4 +2,5 @@
 const contract=require('./contract');
 const semantic=require('./semantic-signature');
 const volatileHud=require('./volatile-hud');
-module.exports={contract,semantic,volatileHud,production_active:false,score_logic_changed:false,random_scoring_changed:false};
+const coordinator=require('./coordinator');
+module.exports={contract,semantic,volatileHud,coordinator,production_active:false,score_logic_changed:false,random_scoring_changed:false};
