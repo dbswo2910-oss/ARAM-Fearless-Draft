4df16819eaa2fb5888dd49ff4b723956ab06fdc5
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 18:32:52 +0900

    stability: expose RANDOM teamScore legacy boundary

--- src/random/pick/index.js
+++ src/random/pick/index.js
@@ -3,4 +3,5 @@ const contract=require('./contract');
 const dna=require('./dna');
 const selection=require('./selection-state');
 const top5=require('./top5');
-module.exports={contract,dna,selection,top5,production_active:false,score_logic_changed:false,random_scoring_changed:false};
+const teamScoreAdapter=require('./team-score-adapter');
+module.exports={contract,dna,selection,top5,teamScoreAdapter,production_active:false,score_logic_changed:false,random_scoring_changed:false};
