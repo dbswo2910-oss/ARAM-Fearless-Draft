c096606ccc42f9ede502c3337c5971bd73bbab01
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 18:34:27 +0900

    stability: expose canonical profile metric shadow

--- src/profile/index.js
+++ src/profile/index.js
@@ -1,4 +1,5 @@
 'use strict';
 const contract=require('./contract');
 const results=require('./results-normalizer');
-module.exports={contract,results,production_active:false,score_logic_changed:false,random_scoring_changed:false};
+const metrics=require('./metrics');
+module.exports={contract,results,metrics,production_active:false,score_logic_changed:false,random_scoring_changed:false};
