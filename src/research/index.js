ddc1cc1f63861265d5102c699da9fbf5c3fbfba0
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 17:06:52 +0900

    stability: expose canonical Research storage shadow

---
+++ src/research/index.js
@@ -0,0 +1,4 @@
+'use strict';
+const contract=require('./contract');
+const storage=require('./storage');
+module.exports={contract,storage,production_active:false,score_logic_changed:false,random_scoring_changed:false};
