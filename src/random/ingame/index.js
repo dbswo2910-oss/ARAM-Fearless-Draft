f971758dba64d00f01c444a61bb316e40e6c48b2
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 18:30:40 +0900

    stability: expose random ingame scheduler and coach render shadows

--- src/random/ingame/index.js
+++ src/random/ingame/index.js
@@ -4,4 +4,6 @@ const semantic=require('./semantic-signature');
 const volatileHud=require('./volatile-hud');
 const coordinator=require('./coordinator');
 const shopPipeline=require('./shop-pipeline');
-module.exports={contract,semantic,volatileHud,coordinator,shopPipeline,production_active:false,score_logic_changed:false,random_scoring_changed:false};
+const coachRender=require('./coach-render');
+const scheduler=require('./scheduler');
+module.exports={contract,semantic,volatileHud,coordinator,shopPipeline,coachRender,scheduler,production_active:false,score_logic_changed:false,random_scoring_changed:false};
