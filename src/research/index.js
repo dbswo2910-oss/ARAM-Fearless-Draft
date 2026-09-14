967f8a1940082ffb21504f00f0a60b44f4131947
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 19:16:50 +0900

    stability: expose canonical Research v0.3.1 sampling shadow

--- src/research/index.js
+++ src/research/index.js
@@ -1,4 +1,5 @@
 'use strict';
 const contract=require('./contract');
 const storage=require('./storage');
-module.exports={contract,storage,production_active:false,score_logic_changed:false,random_scoring_changed:false};
+const sampling=require('./sampling-v031');
+module.exports={contract,storage,sampling,production_active:false,score_logic_changed:false,random_scoring_changed:false};
