878f3b2f99a8e69c447fb53ef405f1172f124e90
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 16:37:49 +0900

    refactor: expose RANDOM IN GAME semantic shadow

---
+++ src/random/ingame/index.js
@@ -0,0 +1,4 @@
+'use strict';
+const contract=require('./contract');
+const semantic=require('./semantic-signature');
+module.exports={contract,semantic,production_active:false,score_logic_changed:false,random_scoring_changed:false};
