370825ab0425ec6d449fd44dfbd16bdb3378b100
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 16:50:22 +0900

    refactor: expose canonical DATA Patch Notes shell shadow

---
+++ src/data/index.js
@@ -0,0 +1,4 @@
+'use strict';
+const contract=require('./contract');
+const patchModeShell=require('./patch-mode-shell');
+module.exports={contract,patchModeShell,production_active:false,score_logic_changed:false,random_scoring_changed:false};
