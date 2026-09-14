3a11b82feaa2f9aa6ccd75a5c7a792d4bbb50e05
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 22:34:07 +0900

    research: export canonical rating engine UI owner

--- src/research/index.js
+++ src/research/index.js
@@ -2,4 +2,7 @@
 const contract=require('./contract');
 const storage=require('./storage');
 const sampling=require('./sampling-v031');
-module.exports={contract,storage,sampling,production_active:false,score_logic_changed:false,random_scoring_changed:false};
+const ratingEngine=require('./rating-engine');
+const uiCore=require('./ui-core');
+const owner=require('./owner');
+module.exports={contract,storage,sampling,ratingEngine,uiCore,owner,production_active:false,automatic_collection:false,b2_manual_only:true,destructive_migration:false,score_logic_changed:false,random_scoring_changed:false};
