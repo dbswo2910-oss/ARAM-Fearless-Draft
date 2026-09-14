09e18950a3d60673eaaa2a10cde89e0d5099df9a
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 16:35:31 +0900

    refactor: expose RANDOM TOP5 enumeration shadow

--- src/random/pick/index.js
+++ src/random/pick/index.js
@@ -2,4 +2,5 @@
 const contract=require('./contract');
 const dna=require('./dna');
 const selection=require('./selection-state');
-module.exports={contract,dna,selection,production_active:false,score_logic_changed:false,random_scoring_changed:false};
+const top5=require('./top5');
+module.exports={contract,dna,selection,top5,production_active:false,score_logic_changed:false,random_scoring_changed:false};
