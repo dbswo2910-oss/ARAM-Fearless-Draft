f25952e74bf20d76a330fb8cf08369347a3504a4
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 18:31:55 +0900

    stability: expose canonical DATA mode and nav shadows

--- src/data/index.js
+++ src/data/index.js
@@ -2,4 +2,6 @@
 const contract=require('./contract');
 const patchModeShell=require('./patch-mode-shell');
 const workspace=require('./workspace');
-module.exports={contract,patchModeShell,workspace,production_active:false,score_logic_changed:false,random_scoring_changed:false};
+const modeController=require('./mode-controller');
+const navigation=require('./navigation');
+module.exports={contract,patchModeShell,workspace,modeController,navigation,production_active:false,score_logic_changed:false,random_scoring_changed:false};
