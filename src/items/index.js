5265ab7a684fd035df518f32d51d390e6ac9ec19
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 16:58:48 +0900

    stability: expose canonical item shadow modules

---
+++ src/items/index.js
@@ -0,0 +1,5 @@
+'use strict';
+const contract=require('./contract');
+const identity=require('./identity');
+const recommendation=require('./recommendation');
+module.exports={contract,identity,recommendation,production_active:false,score_logic_changed:false,random_scoring_changed:false};
