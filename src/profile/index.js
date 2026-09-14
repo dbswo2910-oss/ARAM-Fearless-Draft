5bfb926711c146a109442966b3fcb688a6dfccc5
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 17:02:45 +0900

    stability: expose canonical profile results shadow

---
+++ src/profile/index.js
@@ -0,0 +1,4 @@
+'use strict';
+const contract=require('./contract');
+const results=require('./results-normalizer');
+module.exports={contract,results,production_active:false,score_logic_changed:false,random_scoring_changed:false};
