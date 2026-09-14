5fc6c1268b819549f2b0ea4e3be26233a9a07ecb
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 22:41:48 +0900

    diagnostics: export canonical shell owner

--- src/diagnostics/index.js
+++ src/diagnostics/index.js
@@ -1,4 +1,5 @@
 'use strict';
 const contract=require('./contract');
 const panel=require('./panel');
-module.exports={contract,panel,production_active:false,read_only:true,privacy_safe:true};
+const owner=require('./owner');
+module.exports={contract,panel,owner,production_active:false,read_only:true,privacy_safe:true};
