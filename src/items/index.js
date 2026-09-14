f5630283c84c95a2f9a88cfffec61a37aa6b68aa
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 18:05:47 +0900

    stability: expose canonical item art runtime shadow

--- src/items/index.js
+++ src/items/index.js
@@ -3,5 +3,6 @@ const contract=require('./contract');
 const identity=require('./identity');
 const recommendation=require('./recommendation');
 const artResolver=require('./art-resolver');
+const artRuntime=require('./art-runtime');
 const catalogContract=require('./catalog-contract');
-module.exports={contract,identity,recommendation,artResolver,catalogContract,production_active:false,score_logic_changed:false,random_scoring_changed:false};
+module.exports={contract,identity,recommendation,artResolver,artRuntime,catalogContract,production_active:false,score_logic_changed:false,random_scoring_changed:false};
