7339c0bfd02daa0268514fabc2c77b9da062cc92
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-14 16:45:42 +0900

    refactor: expose RANDOM IN GAME shop pipeline shadow

--- src/random/ingame/index.js
+++ src/random/ingame/index.js
@@ -3,4 +3,5 @@ const contract=require('./contract');
 const semantic=require('./semantic-signature');
 const volatileHud=require('./volatile-hud');
 const coordinator=require('./coordinator');
-module.exports={contract,semantic,volatileHud,coordinator,production_active:false,score_logic_changed:false,random_scoring_changed:false};
+const shopPipeline=require('./shop-pipeline');
+module.exports={contract,semantic,volatileHud,coordinator,shopPipeline,production_active:false,score_logic_changed:false,random_scoring_changed:false};
