c7169044d6e587e861e991f3c3fc0cdaada00b27
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-16 15:26:03 +0900

    research: export dual shadow owner

--- src/research/index.js
+++ src/research/index.js
@@ -4,5 +4,6 @@ const storage=require('./storage');
 const sampling=require('./sampling-v031');
 const ratingEngine=require('./rating-engine');
 const uiCore=require('./ui-core');
+const dualShadow=require('./dual-shadow');
 const owner=require('./owner');
-module.exports={contract,storage,sampling,ratingEngine,uiCore,owner,production_active:false,automatic_collection:false,b2_manual_only:true,destructive_migration:false,score_logic_changed:false,random_scoring_changed:false};
+module.exports={contract,storage,sampling,ratingEngine,uiCore,dualShadow,owner,production_active:false,automatic_collection:false,b2_manual_only:true,destructive_migration:false,score_logic_changed:false,random_scoring_changed:false};
