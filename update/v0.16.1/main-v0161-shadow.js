085fda6a27415e114d33f6bcd624f8fea133c1e0
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-16 23:18:57 +0900

    fix(rating): keep canonical preload route in v0.16.1 successor

--- update/v0.16.1/main-v0161-shadow.js
+++ update/v0.16.1/main-v0161-shadow.js
@@ -20,7 +20,7 @@ if(!src.includes(pinOld))throw new Error('v0.16.1 successor contract mismatch: s
 src=src.replace(pinOld,pinNew);
 
 const compileOld="const src=patchSuccessorSource(fs.readFileSync(basePath,'utf8'));\nmodule._compile(src,__filename);";
-const compileNew="let src=patchSuccessorSource(fs.readFileSync(basePath,'utf8'));\nconst __preloadOld=\"path.join(__dirname,'preload.js')\";\nconst __preloadNew=\"path.join(__dirname,'preload-v0161-shadow.js')\";\nif(!src.includes(__preloadOld))throw new Error('v0.16.1 successor contract mismatch: preload route missing');\nsrc=src.replace(__preloadOld,__preloadNew).replaceAll('0.16.0','0.16.1');\nmodule._compile(src,__filename);";
+const compileNew="const src=patchSuccessorSource(fs.readFileSync(basePath,'utf8')).replaceAll('0.16.0','0.16.1');\nmodule._compile(src,__filename);";
 if(!src.includes(compileOld))throw new Error('v0.16.1 successor contract mismatch: compile anchor missing');
 src=src.replace(compileOld,compileNew);
 module._compile(src,__filename);
