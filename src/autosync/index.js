3d615328a16a7b10d212d555b3e14f3b4f04bd67
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-17 12:16:40 +0900

    refactor: expose canonical AutoSync feature owners

--- src/autosync/index.js
+++ src/autosync/index.js
@@ -1,4 +1,8 @@
 'use strict';
 const concurrency=require('./concurrency');
+const queue=require('./queue');
+const ccImpact=require('./cc-impact');
+const missionTimeline=require('./mission-timeline');
 const telemetry=require('./telemetry');
-module.exports={...concurrency,telemetry,installTelemetry:telemetry.install};
+function installHistoryOwners(coreMod){queue.install(coreMod);ccImpact.install(coreMod);missionTimeline.install(coreMod);telemetry.install(coreMod);return coreMod}
+module.exports={...concurrency,queue,ccImpact,missionTimeline,telemetry,installHistoryOwners,installQueue:queue.install,installCcImpact:ccImpact.install,installMissionTimeline:missionTimeline.install,installTelemetry:telemetry.install};
