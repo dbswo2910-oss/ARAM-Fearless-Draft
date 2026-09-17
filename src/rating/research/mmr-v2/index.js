f87ea784db7d6f80192b58f815d89ea78bebf8c5
dbswo2910-oss <dbswo2910@gmail.com>
2026-09-18 03:13:27 +0900

    research: expose MMR v2 measurement API

--- src/rating/research/mmr-v2/index.js
+++ src/rating/research/mmr-v2/index.js
@@ -4,12 +4,14 @@ const dataset = require('./dataset');
 const model = require('./model');
 const metrics = require('./metrics');
 const evaluator = require('./evaluator');
+const measure = require('./measure');
 
 module.exports = Object.freeze({
   ...dataset,
   ...model,
   ...metrics,
   ...evaluator,
+  ...measure,
   subsystem: 'aram-mmr-v2-global-latent-shadow',
   queueId: 450,
   researchOnly: true,
